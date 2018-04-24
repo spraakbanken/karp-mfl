0e23b5e9f31ca1a52628b145ffcff4e6a99db130
MalinAhlberg <malin.ahlberg@gu.se>
2018-04-24 11:02:18 +0200

    compile_wf, identifier is always first

--- mflbackend/config/saolp_convert.py
+++ mflbackend/config/saolp_convert.py
@@ -10,8 +10,8 @@ def make_overview(obj):
     pos = obj.get("partOfSpeech", "")
     paradigm = obj.get("paradigm", "")
     identifier = obj.get("identifier", "")
-    out = [base, identifier, pos, bklass, paradigm]
-    fields = ["baseform", "identifier", "partOfSpeech", "bklass", "paradigm"]
+    out = [identifier, base, pos, bklass, paradigm]
+    fields = ["identifier", "baseform", "partOfSpeech", "bklass", "paradigm"]
     return out, fields
 
 
