56339e9a87e411bd14a996e8f0d478b4b1b14440
Funktionskonto <fkkarp@k2.spraakdata.gu.se>
2018-06-25 12:00:48 +0200

    sbserver runnig with log

--- mflbackend/makedump.py
+++ mflbackend/makedump.py
@@ -1,3 +1,8 @@
+import logging
+import sys
+
+# set logging level before importing anything else
+logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 import src.backend
 
 src.backend.prepare_restart()
